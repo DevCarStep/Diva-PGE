7f5c0fc81e8877b97cd19e7b9312d45a84a659e0
CarStep <carstep9823@gmail.com>
2025-10-15 10:20:45 +0300

    Init

---
+++ ClassLibrary1/MathUtils.cs
@@ -0,0 +1,22 @@
+﻿// MyUnityPlugin/MathUtils.cs
+using System.Numerics;
+using System;
+using UnityEngine;
+
+namespace MyUnityPlugin
+{
+    public static class MathUtils
+    {
+        public static float Clamp01(float value)
+        {
+            return Mathf.Clamp01(value);
+        }
+
+        public static UnityEngine.Vector3 RandomPointInCircle(float radius)
+        {
+            float angle = UnityEngine.Random.Range(0f, 360f);
+            float dist = UnityEngine.Random.Range(0f, radius);
+            return new UnityEngine.Vector3(Mathf.Cos(angle) * dist, 0, Mathf.Sin(angle) * dist);
+        }
+    }
+}
