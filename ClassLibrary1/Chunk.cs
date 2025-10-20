cc65c04ec9e217dd0ef2be62daaa35eecce5a704
CarStep <carstep9823@gmail.com>
2025-10-20 21:11:12 +0300

    Code cheating started
    I won't hide it

---
+++ ClassLibrary1/Chunk.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using UnityEngine;
+namespace DivaPGE
+{
+    public class Chunk : MonoBehaviour
+    {
+        public Transform Begin;
+        public Transform End;
+        private void Start()
+        {
+
+        }
+    }
+}
