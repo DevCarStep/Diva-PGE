030080717ffd5637681992780b438c47614caa5d
CarStep <carstep9823@gmail.com>
2025-10-23 20:58:11 +0300

    It's finnaly just works, but I did nothing

--- DivaPGE/ChunkPlacer.cs
+++ DivaPGE/ChunkPlacer.cs
@@ -9,19 +9,21 @@ namespace DivaPGE
     {
         public Chunk[] ChunkPrefabs;
         private List<Chunk> spawnedChunks = new List<Chunk>();
+        public Chunk FirstChunk;
         private void Start()
         {
-
+            spawnedChunks.Add(FirstChunk);
+            for (int i = 0; i < 3; i++)
+                SpawnChunk();
         }
         private void Update()
         {
-
+            
         }
         private void SpawnChunk()
         {
             Chunk newChunk = Instantiate(ChunkPrefabs[UnityEngine.Random.Range(0, ChunkPrefabs.Length)]);
             newChunk.transform.position = spawnedChunks[spawnedChunks.Count - 1].End.position - newChunk.Begin.localPosition;
-            spawnedChunks.Add(newChunk);
             spawnedChunks.Add(newChunk);
         }
     }
