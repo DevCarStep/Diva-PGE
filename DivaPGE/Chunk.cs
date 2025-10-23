1c632e87143027613c4a8f0a40be4bd99c6fdef9
CarStep <carstep9823@gmail.com>
2025-10-23 20:14:24 +0300

    Recreation

---
+++ DivaPGE/Chunk.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DivaPGE
+{
+    public class Chunk : MonoBehaviour
+    {
+        public Transform Begin;
+        public Transform End;
+        private void Start()
+        {
+
+        }
+    }
+}
---
+++ DivaPGE/ChunkPlacer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace DivaPGE
+{
+    public class ChunkPlacer : MonoBehaviour
+    {
+        public Chunk[] ChunkPrefabs;
+        private List<Chunk> spawnedChunks = new List<Chunk>();
+        private void Start()
+        {
+
+        }
+        private void Update()
+        {
+
+        }
+        private void SpawnChunk()
+        {
+            Chunk newChunk = Instantiate(ChunkPrefabs[UnityEngine.Random.Range(0, ChunkPrefabs.Length)]);
+            newChunk.transform.position = spawnedChunks[spawnedChunks.Count - 1].End.position - newChunk.Begin.localPosition;
+            spawnedChunks.Add(newChunk);
+            spawnedChunks.Add(newChunk);
+        }
+    }
+}
