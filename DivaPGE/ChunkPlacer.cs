c8b26de1bfc08b7a5d8156e19fb675ab15551618
CarStep <carstep9823@gmail.com>
2025-10-27 21:04:07 +0300

    Tryed to do something, I'm need help

--- DivaPGE/ChunkPlacer.cs
+++ DivaPGE/ChunkPlacer.cs
@@ -17,6 +17,8 @@ namespace DivaPGE
         public int TotalAmountOfElements;
         private void Start()
         {
+            Chunk BeginingChunk = Instantiate(FirstChunk);
+            BeginingChunk.transform.position = gameObject.transform.position;
             spawnedChunks.Add(FirstChunk);
             for (int i = 0; i < TotalAmountOfElements; i++)
                 SpawnChunk();
@@ -28,19 +30,33 @@ namespace DivaPGE
         private void SpawnChunk()
         {
             Chunk newChunk;
+            int previousRandomizedPoint = UnityEngine.Random.Range(0, spawnedChunks[spawnedChunks.Count - 1].Points.Length);
+            int newRandomizedPoint;
 
             if (spawnedChunks.Count == 1)
             {
                 newChunk = Instantiate(ChunkPrefabs[UnityEngine.Random.Range(0, ChunkPrefabs.Length)]);
+                newRandomizedPoint = UnityEngine.Random.Range(0, newChunk.Points.Length);
                 newChunk.transform.position = spawnedChunks[0].Begin.position - newChunk.Points[UnityEngine.Random.Range(0, newChunk.Points.Length)].localPosition;
+                if (Vector3.Angle(newChunk.Points[newRandomizedPoint].transform.position, spawnedChunks[0].Begin.transform.position) != 180f)
+                {
+                    float differenceAngle = Vector3.Angle(newChunk.Points[newRandomizedPoint].transform.position, spawnedChunks[0].Begin.transform.position);
+                    newChunk.transform.RotateAround(newChunk.Points[newRandomizedPoint].transform.position, Vector3.up, (differenceAngle) + 180f);
+                    if ((differenceAngle) + 180f != 180f)
+                        Debug.Log($"Поворот не удался, угол остался {Vector3.Angle(newChunk.Points[newRandomizedPoint].transform.position, spawnedChunks[0].Begin.transform.position)} градусов");
+                }
                 spawnedChunks.Add(newChunk);
             }
             newChunk = Instantiate(ChunkPrefabs[UnityEngine.Random.Range(0, ChunkPrefabs.Length)]);
-            int previousRandomizedPoint = UnityEngine.Random.Range(0, spawnedChunks[spawnedChunks.Count - 1].Points.Length);
-            int newRandomizedPoint = UnityEngine.Random.Range(0, newChunk.Points.Length);
+            newRandomizedPoint = UnityEngine.Random.Range(0, newChunk.Points.Length);
             newChunk.transform.position = spawnedChunks[spawnedChunks.Count - 1].Points[UnityEngine.Random.Range(0, spawnedChunks[spawnedChunks.Count-1].Points.Length)].position - newChunk.Points[newRandomizedPoint].localPosition;
-            while (newChunk.Points[newRandomizedPoint].transform.eulerAngles - spawnedChunks[spawnedChunks.Count - 1].Points[previousRandomizedPoint].transform.eulerAngles != new Vector3(2,0,0))
-                newChunk.transform.RotateAround(newChunk.transform.position, newChunk.Points[newRandomizedPoint].position, 90f);
+            if (Vector3.Angle(newChunk.Points[newRandomizedPoint].transform.position, spawnedChunks[spawnedChunks.Count - 1].Points[previousRandomizedPoint].transform.position) != 180f)
+            {
+                float differenceAngle = Vector3.Angle(newChunk.Points[newRandomizedPoint].transform.position, spawnedChunks[spawnedChunks.Count - 1].Points[previousRandomizedPoint].transform.position);
+                newChunk.transform.RotateAround(newChunk.Points[newRandomizedPoint].transform.position, Vector3.up, (differenceAngle)+180f);
+                if ((differenceAngle) + 180f != 180f)
+                    Debug.Log($"Поворот не удался, угол остался {Vector3.Angle(newChunk.Points[newRandomizedPoint].transform.position, spawnedChunks[spawnedChunks.Count - 1].Points[previousRandomizedPoint].transform.position)} градусов");
+            }    
             spawnedChunks.Add(newChunk);
         }
     }
